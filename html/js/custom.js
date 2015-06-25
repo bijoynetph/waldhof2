f9e784f9210c08261d4ca8980fc5c957a6103f85
Bijoy Sen <bijoy.sen@netzrezepte.de>
2015-06-25 19:52:56 +0530

    header

--- html/js/custom.js
+++ html/js/custom.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
-	$('.megaMenu .rightSec ul li:odd').addClass('evenMenu');
-	$('.buttons').parent().addClass('speialElement');
+	var mobNav = $('nav.mainMenu').clone().addClass('mobileNav').removeClass('mainMenu');
+	$('nav.mainMenu').after(mobNav);
+	$('.mobileNav .leftSec, .mobileNav .blLogo').remove();
+	$('.mobileNav ul li a span').prepend('» ');
+	$('nav.mainMenu .megaMenu .rightSec ul li:odd').addClass('evenMenu');
+	$('.logo').before('<span class="mobileIcon"></span>');
+	$(document).on('click', '.mobileIcon', function() {
+		$('nav.mobileNav').slideToggle(500);
+	});
+	$('.mobileNav ul li a').click(function () {
+        if ($(this).closest('li').children('.megaMenu').is(':hidden') == true) {
+            $(this).closest('li').children('.megaMenu').slideDown(500);
+            return false
+        }
+    })
+});
+
+$(window).resize(function(event) {
+	
 });
