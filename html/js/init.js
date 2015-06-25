a8a3ac216dff6f7ce533c0ca9a0757337399b3a9
soumen paul <soumen.paul@netzrezepte.de>
2015-06-25 10:58:46 +0530

    added content

---
+++ html/js/init.js
@@ -0,0 +1,108 @@
+// JavaScript Document
+$(document).ready(function(e) {
+	// First
+    $('ul > li:first-child, ol > li:first-child, dl > dt:first-child, dl > dd:first-child, .group .column:first-child, .footer-menu > ul:first-child, #footer-nav ul:first-child, .section .teaser:first-child').addClass('first');
+	
+	// Last
+	$('ul > li:last-child, ol > li:last-child, dl > dt:last-child, dl > dd:last-child, .group .column:last-child, .article:last-child, .article p:last-child, .txt p:last-child, #footer-nav ul:last-child, .section .teaser:last-child').addClass('last');
+	
+	
+	// Scroll to Top
+	$(".backToTop").click(function(e) {
+	    $("html, body").animate({ scrollTop: 0 }, "slow");
+	    e.preventDefault();
+	});
+	
+
+	var imgHight = $('.box-height .vertical img').height();
+	 $('.multibox-col .box-height').css('height', imgHight);
+
+	
+	if($(window).width() > 640){
+		$('.header-left-bottom li').hover(function() {
+          $(this).addClass('highlight');
+	  	},
+		function(){
+		  $(this).removeClass('highlight');
+		});	
+	}
+
+	//equal hight block
+ 
+	function equalHeight(group) {
+		var tallest = 0;
+		group.each(function() {
+			var thisHeight = $(this).height();
+			if(thisHeight > tallest) {
+				tallest = thisHeight;
+			}
+		});
+		group.height(tallest);
+	}
+	
+		equalHeight($(".col-4-container .teaser-item h4"));
+		equalHeight($(".col-4-footer .teaser-item"));
+		
+		
+  // responsive 
+	$(".header-left-bottom .menu-icon").click(function(){
+		$(".header-left-bottom ul.main-menu").slideToggle();	
+	});
+	
+
+	
+	$('.multibox-col .vertical img').parents('.vertical').find('.block > .csc-textpic').unwrap();
+
+
+	if($(window).width() < 641){
+		$('.news-block .col').wrapAll('<div class="extra-wrapper"></div>').wrapAll('<ul class="bxslider"></ul>').wrap('<li></li>');
+		$('ul.main-menu li.drop a').click(function(e) {
+			$(this).next().slideToggle();
+			e.preventDefault();
+		});
+		$('.copyright .col.block-4').insertAfter('.copyright  .col.block-8')
+	};
+
+	$('.bxslider').bxSlider();
+        
+       
+    // magnific popup
+
+	 jQuery.extend(true, jQuery.magnificPopup.defaults, {  
+	     iframe: {
+	         patterns: {
+	            youtube: {
+	               index: 'youtube.com/', 
+	               id: 'v=', 
+	               src: 'https://www.youtube.com/embed/W4mnEKjhEKc' 
+	           }
+	         }
+	     }
+	 });
+
+
+	 jQuery('a.popup').magnificPopup({
+	         disableOn: 700,
+	         type: 'iframe',
+	         mainClass: 'mfp-fade',
+	         removalDelay: 160,
+	         preloader: false,
+	         fixedContentPos: true,
+	         titleSrc: 'title'
+
+	     });
+
+
+
+
+
+
+
+	});
+
+
+$(window).resize(function() {
+	var imgHight = $('.box-height .vertical img').height();
+	 $('.multibox-col .box-height').css('height', imgHight);
+
+});
