f98eac3cb9dc837bf52d8f8788e017c293680dc1
Bijoy Sen <bijoy.sen@netzrezepte.de>
2015-06-30 09:48:22 +0530

    modification

--- html/js/custom.js
+++ html/js/custom.js
@@ -1,3 +1,4 @@
+document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':5001/livereload.js?snipver=1"></' + 'script>')
 function mobMenu(){
 	var winWidth = $(window).width();
 	if (winWidth > 640) {
